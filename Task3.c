9924db5d6d30911115397389d71a31b9e7212a56
AmmoniumThioglycolate <109291865+AmmoniumThioglycolate@users.noreply.github.com>
2024-02-21 22:21:05 +0000

    Update Task3.c

--- Task3.c
+++ Task3.c
@@ -1,6 +1,3 @@
-#include <msString.h>
-
-
 struct stringStruct{
         long int length;
         char * string;
@@ -8,15 +5,58 @@ struct stringStruct{
 extern msString  msSetString (char *str){
    //the char str saves perfectly as a string so not much to worry about in that regard
   //an msString is meant to be a pointer that leads to a memory address that stores a long int and characters. Maybe use an array
+        static struct stringStruct array[100];
+        static int numarray=0;
+        numarray += 1;
         struct stringStruct myStr;
         myStr.length = strlen(str);
-        if (!(myStr.string = (char *)malloc(300*sizeof(char)))){
+        if (!(myStr.string = (char *)malloc(30*sizeof(char)))){
                 printf("Insufficient memeory\n");
                 exit(1);
         }
         strcpy(myStr.string,str);
-        free(myStr.string);
-        //I'm return the address of myStr as msString will just point to that
-        return &myStr
+        /*success. Now return a pointer*/
+        array[numarray]=myStr;
+        return(&array[numarray]); }
+}
+extern char *msGetString (msString str){
+        struct stringStruct  myStr = *(struct stringStruct *)ptr;
+        return(myStr.string);
+}
+extern void  msCopy (msString *destination, msString source){
+        if (!((*destination)=(void *)malloc(sizeof(source)+1))){
+                printf("Insufficient memory\n");
+                exit(1);
+        }
+        struct stringStruct destStr = *(struct stringStruct *)destination;
+        struct stringStruct sourceStr = *(struct stringStruct *)source;
+        destStr.string = sourceStr.string;
+        free(*destination);
+}
+extern void msConcatenate (msString *destStr, msString sourceStr){
+        
+}
+extern long int  msLength (msString str){
+        struct stringStruct  myStr = *(struct stringStruct *)str;
+        return(myStr.length);
+}
+extern int  msCompare       (msString str1, msString str2){
+        int i = strcmp(msGetString(ptr1),msGetString(ptr2));
+        if (i == 0){
+                return(0);
+        } else {return(1);}
+}
+extern int       msCompareString (msString str1, char *standardStr){
+        struct stringStruct  myStr = *(struct stringStruct *)str1;
+        int i = strcmp(myStr.string,standardStr);
+        if (i == 0){
+                return(0);
+        } else {return(1);}
+}
 
+
+static void      msError         (char *standardStr){
+        printf("there has been an error with the string : %s\n",standardStr)
+        exit(1);
+ 
 }
