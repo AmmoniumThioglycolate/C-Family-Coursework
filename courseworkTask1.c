419af4efeb1fafd279538c3d4de2325280930355
AmmoniumThioglycolate <109291865+AmmoniumThioglycolate@users.noreply.github.com>
2024-02-22 01:46:09 +0000

    Update courseworkTask1.c

--- courseworkTask1.c
+++ courseworkTask1.c
@@ -31,3 +31,43 @@ void printBytes(void* ptr, int numBytes){
 
 
 }
+
+
+
+
+
+
+
+
+#include <stdio.h>
+
+void printBytes(void*,int);
+
+int main(){
+
+        int array2[]={9,3,4,2,4,23,37,53};
+        void *p;
+        p = &array2[1];
+        float *p2;
+        p2 = p;
+
+
+        printf("The value of array2[1] is %f and a float as an integer : %d.The memory address of p is %p\n and th next memory address is %x. The value of p is %d and of p2 is %f\n",array2[1],(float)array2[1],p,&array2[2],*(int *)p,*p2);
+        printBytes(p,7);
+
+
+
+        return 0;
+}
+
+void printBytes(void* ptr, int numBytes){
+        printf("Starting at memory address %p\n", ptr);
+        unsigned short int i;
+        for (i=0;i<numBytes; i++){
+                printf("%03d: %3d\n",(i+1),*(int *)ptr);
+                ptr++;
+        }
+
+
+
+}
