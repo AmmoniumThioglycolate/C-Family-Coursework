f2d24c3224078e3773ddc32d39addd3718cfd329
AmmoniumThioglycolate <109291865+AmmoniumThioglycolate@users.noreply.github.com>
2024-02-22 10:40:45 +0000

    Update Task3.c

--- Task3.c
+++ Task3.c
@@ -1,34 +1,39 @@
+#include <msString.h>
+
+
 struct stringStruct{
-        long int length;
+        long int * length;
         char * string;
 };
 extern msString  msSetString (char *str){
-   //the char str saves perfectly as a string so not much to worry about in that regard
-  //an msString is meant to be a pointer that leads to a memory address that stores a long int and characters. Maybe use an array
-        static struct stringStruct array[100];
+   /*the char str saves perfectly as a string so not much to worry about in that regard*/
+  /*an msString is meant to be a pointer that leads to a memory address that stores a long int and characters. Maybe use an array
+*/      static struct stringStruct array[100];
         static int numarray=0;
         numarray += 1;
         struct stringStruct myStr;
-        myStr.length = strlen(str);
+        if (!(myStr.length = (long int *)malloc(sizeof(long int)))){
+                printf("Insufficient memeory\n");
+                exit(1);
+        }
+        *(long int *)myStr.length = strlen(str);
         if (!(myStr.string = (char *)malloc(30*sizeof(char)))){
                 printf("Insufficient memeory\n");
                 exit(1);
         }
+
         strcpy(myStr.string,str);
         /*success. Now return a pointer*/
         array[numarray]=myStr;
-        return(&array[numarray]); }
-}
+        return(&array[numarray]);
+ }
+
 extern char *msGetString (msString str){
-        struct stringStruct  myStr = *(struct stringStruct *)ptr;
+        struct stringStruct  myStr = *(struct stringStruct *)str;
         return(myStr.string);
 }
 extern void  msCopy (msString *destination, msString source){
         void * ptr;
-        if (!((ptr)=(void *)malloc(sizeof(source)+1))){
-                printf("Insufficient memory\n");
-                exit(1);
-        }
         if (*destination == NULL){
                 *destination = msSetString("");
         }
@@ -38,13 +43,12 @@ extern void  msCopy (msString *destination, msString source){
         struct stringStruct sourceStr = *(struct stringStruct *)source;
         strcpy(destStr.string,sourceStr.string);
         *(long int *)destStr.length = strlen(sourceStr.string);
+
+
 }
 extern void msConcatenate (msString *destination, msString source){
         void * ptr;
-        if (!((ptr)=(void *)malloc(sizeof(source)+1))){
-                printf("Insufficient memory\n");
-                exit(1);
-        }
+
         if (*destination == NULL){
                 *destination = msSetString("");
         }
@@ -57,18 +61,24 @@ extern void msConcatenate (msString *destination, msString source){
         *(long int *)destStr.length = *(long int*)destStr.length + *(long int *)sourceStr.length;
 
 
+
 }
 extern long int  msLength (msString str){
         struct stringStruct  myStr = *(struct stringStruct *)str;
-        return(myStr.length);
+        long int number = *(long int *)myStr.length;
+
+        return(number);
 }
 extern int  msCompare       (msString str1, msString str2){
-        int i = strcmp(msGetString(ptr1),msGetString(ptr2));
+        int i = strcmp(msGetString(str1),msGetString(str2));
         if (i == 0){
                 return(0);
-        } else {return(1);}
+        }
+        else {
+        return(1);
+        }
 }
-extern int       msCompareString (msString str1, char *standardStr){
+extern int msCompareString (msString str1, char *standardStr){
         struct stringStruct  myStr = *(struct stringStruct *)str1;
         int i = strcmp(myStr.string,standardStr);
         if (i == 0){
@@ -78,7 +88,7 @@ extern int       msCompareString (msString str1, char *standardStr){
 
 
 static void      msError         (char *standardStr){
-        printf("there has been an error with the string : %s\n",standardStr)
+        printf("there has been an error with the string : %s\n",standardStr);
         exit(1);
- 
+
 }
